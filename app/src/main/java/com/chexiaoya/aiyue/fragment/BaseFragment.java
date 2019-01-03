67371f5a02f0ddb729bbf236d85568d73323ead0
xcb <xiecb@gzyitop.com>
2019-01-03 14:58:14 +0800

    新建fragment

---
+++ app/src/main/java/com/chexiaoya/aiyue/fragment/BaseFragment.java
@@ -0,0 +1,51 @@
+package com.chexiaoya.aiyue.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by xcb on 2018/12/29.
+ */
+public abstract class BaseFragment extends Fragment {
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    public abstract int getLatout();
+
+    public abstract void initData();
+}
---
+++ app/src/main/java/com/chexiaoya/aiyue/fragment/JianDanFragment.java
@@ -0,0 +1,7 @@
+package com.chexiaoya.aiyue.fragment;
+
+/**
+ * Created by xcb on 2018/12/29.
+ */
+public class JianDanFragment {
+}
