af35cacb4c4d49dbd5ae05303c07374bbb801610
xcb <xiecb@gzyitop.com>
2019-01-22 17:31:13 +0800

    带头部尾部的adapter

---
+++ app/src/main/java/com/chexiaoya/aiyue/RecyclerView/adapter/WrapRecyclerAdapter.java
@@ -0,0 +1,188 @@
+package com.chexiaoya.aiyue.RecyclerView.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseArray;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.orhanobut.logger.Logger;
+
+/**
+ * 可以添加头部和底部的Adapter
+ * Created by xcb on 2019/1/22.
+ */
+public class WrapRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    //存放头部和底部的view 的集合
+    private SparseArray<View> mHeaderViews;
+    private SparseArray<View> mFooterViews;
+
+    // 基本的头部类型开始位置  用于viewType
+    private static int BASE_ITEM_TYPE_HEADER = 10000000;
+    // 基本的底部类型开始位置  用于viewType
+    private static int BASE_ITEM_TYPE_FOOTER = 20000000;
+
+    // 列表的Adapter
+    private RecyclerView.Adapter mAdapter;
+
+    public WrapRecyclerAdapter(RecyclerView.Adapter mAdapter) {
+        this.mAdapter = mAdapter;
+        mHeaderViews = new SparseArray<>();
+        mFooterViews = new SparseArray<>();
+    }
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (isHeadViewType(viewType)) {
+            View headerView = mHeaderViews.get(viewType);
+            return createHeaderFooterViewHolder(headerView);
+        } else if (isFooterViewType(viewType)) {
+            View footerView = mFooterViews.get(viewType);
+            return createHeaderFooterViewHolder(footerView);
+        } else {
+            return mAdapter.onCreateViewHolder(parent, viewType);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Logger.e("mHeaderViews,size = " + mHeaderViews.size());
+        if (isHeadViewType(position) || isFooterViewType(position)) {
+            Logger.e("头部");
+            return;
+        }
+        position = position - mHeaderViews.size();
+        mAdapter.onBindViewHolder(holder, position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mAdapter.getItemCount() + mFooterViews.size() + mHeaderViews.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (isHeaderPosition(position)) {
+            // 直接返回position位置的key
+            return mHeaderViews.keyAt(position);
+        }
+        if (isFooterPosition(position)) {
+            // 直接返回position位置的key
+            position = position - mHeaderViews.size() - mAdapter.getItemCount();
+            return mFooterViews.keyAt(position);
+        }
+        // 返回列表Adapter的getItemViewType
+        position = position - mHeaderViews.size();
+        return mAdapter.getItemViewType(position);
+    }
+
+    /**
+     * 是不是顶部类型
+     */
+    private boolean isHeadViewType(int viewType) {
+        int position = mHeaderViews.indexOfKey(viewType);
+        Logger.d("position = " + position + ">>viewType = " + viewType);
+        return position >= 0;
+    }
+
+    /**
+     * 是不是底部类型
+     */
+    private boolean isFooterViewType(int viewType) {
+        int position = mFooterViews.indexOfKey(viewType);
+        return position >= 0;
+    }
+
+    /**
+     * 是不是底部位置
+     */
+    private boolean isFooterPosition(int position) {
+        return position >= (mHeaderViews.size() + mAdapter.getItemCount());
+    }
+
+    /**
+     * 是不是头部位置
+     */
+    private boolean isHeaderPosition(int position) {
+        return position < mHeaderViews.size();
+    }
+
+    /**
+     * 创建顶部或者底部的viewHolder
+     */
+    private RecyclerView.ViewHolder createHeaderFooterViewHolder(View view) {
+        return new RecyclerView.ViewHolder(view) {
+        };
+    }
+
+    /**
+     * 获取列表的Adapter
+     */
+    private RecyclerView.Adapter getAdapter() {
+        return mAdapter;
+    }
+
+    /**
+     * 头部添加
+     */
+    public void addHeaderView(View view) {
+        int position = mHeaderViews.indexOfValue(view);
+        if (position < 0) {
+            mHeaderViews.put(BASE_ITEM_TYPE_HEADER++, view);
+        }
+        notifyDataSetChanged();
+    }
+
+
+    /**
+     * 底部添加
+     */
+    public void addFooterView(View view) {
+        int position = mFooterViews.indexOfValue(view);
+        if (position < 0) {
+            mHeaderViews.put(BASE_ITEM_TYPE_FOOTER++, view);
+        }
+        notifyDataSetChanged();
+    }
+
+    /**
+     * 移除头部
+     */
+    public void removeHeaderView(View view) {
+        int index = mHeaderViews.indexOfValue(view);
+        if (index < 0) return;
+        mHeaderViews.removeAt(index);
+        notifyDataSetChanged();
+    }
+
+    /**
+     * 移除底部
+     */
+    public void removeFooterView(View view) {
+        int index = mFooterViews.indexOfValue(view);
+        if (index < 0) return;
+        mFooterViews.removeAt(index);
+        notifyDataSetChanged();
+    }
+
+
+    /**
+     * 解决GridLayoutManager添加头部和底部不占用一行的问题
+     */
+    public void adjustSpanSize(RecyclerView recycler) {
+        if (recycler.getLayoutManager() instanceof GridLayoutManager) {
+            final GridLayoutManager layoutManager = (GridLayoutManager) recycler.getLayoutManager();
+            layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+                @Override
+                public int getSpanSize(int position) {
+                    boolean isHeaderOrFooter =
+                            isHeaderPosition(position) || isFooterPosition(position);
+                    return isHeaderOrFooter ? layoutManager.getSpanCount() : 1;
+                }
+            });
+        }
+    }
+}
